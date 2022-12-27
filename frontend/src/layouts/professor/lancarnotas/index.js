13f0b715e371004a8714367d47c3e8ca01b09aa6
Luídne Mota <luidne.mota@foursys.com.br>
2022-12-26 23:25:47 -0300

    Página inicial da disciplina e seus bimestres.

--- frontend/src/layouts/professor/lancarnotas/index.js
+++ frontend/src/layouts/professor/lancarnotas/index.js
@@ -5,19 +5,49 @@ import {useState} from "react";
 // @mui material components
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
+import {Tab, Tabs} from "@mui/material";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import MDInput from "components/MDInput";
 
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <MDBox sx={{ p: 3 }}>
+                    <MDTypography>{children}</MDTypography>
+                </MDBox>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
 function Tables() {
-    const [searchText, setSearchText] = useState("");
+    const [value, setValue] = useState(0);
+    const handleChange = (e, newValue) => {
+        setValue(newValue);
+    };
 
     return (
         <DashboardLayout>
@@ -42,7 +72,26 @@ function Tables() {
                             </MDBox>
                             <MDBox pt={3}>
                                 <MDBox px={2}>
-
+                                    <MDBox>
+                                        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                                            <Tab label="1º Bimeste" {...a11yProps(0)} />
+                                            <Tab label="2º Bimestre" {...a11yProps(1)} />
+                                            <Tab label="3º Bimestre" {...a11yProps(2)} />
+                                            <Tab label="4º Bimestre" {...a11yProps(3)} />
+                                        </Tabs>
+                                    </MDBox>
+                                    <TabPanel value={value} index={0}>
+                                        Lista dos alunos aqui com o campo das notas do 1º Bimeste
+                                    </TabPanel>
+                                    <TabPanel value={value} index={1}>
+                                        Lista dos alunos aqui com o campo das notas do 2º Bimeste
+                                    </TabPanel>
+                                    <TabPanel value={value} index={2}>
+                                        Lista dos alunos aqui com o campo das notas do 3º Bimeste
+                                    </TabPanel>
+                                    <TabPanel value={value} index={3}>
+                                        Lista dos alunos aqui com o campo das notas do 4º Bimeste
+                                    </TabPanel>
                                 </MDBox>
                             </MDBox>
                         </Card>
